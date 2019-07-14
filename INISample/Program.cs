c5139a3169a7cce9e3614156515f51c60acfaf2c
Takashi Kakinuma <kakinuma.takashi@homes.ecweb.jp>
2019-07-14 13:54:52 +0900

    DLLImport追加

--- INISample/Program.cs
+++ INISample/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,25 @@ namespace INISample
 {
     class Program
     {
+        [DllImport("kernel32.dll")]
+        private static extern int GetPrivateProfileString(
+            string lpApplicationName,
+            string lpKeyName,
+            string lpDefault,
+            StringBuilder lpReturnedstring,
+            int nSize,
+            string lpFileName);
+
+        [DllImport("kernel32.dll")]
+        private static extern int WritePrivateProfileString(
+            string lpApplicationName,
+            string lpKeyName,
+            string lpstring,
+            string lpFileName);
+
         static void Main(string[] args)
         {
+            System.Diagnostics.Debug.WriteLine("Start");
         }
     }
 }
